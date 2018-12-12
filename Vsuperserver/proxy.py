739cc68f5540dc2835d7fadb21c87fd58592ba45
h47er <h47er@yahoo.com>
2018-12-12 17:04:48 +0600

    description on the site

--- Vsuperserver/proxy.py
+++ Vsuperserver/proxy.py
@@ -8,16 +8,19 @@ proxy_socket.bind(("", 8888))
 
 def proxy_handle():
 	while True:
-		data, addr = proxy_socket.recvfrom(5000)
-		user_handler = UsersHandlers.get_by_ip(addr[0])
+		try:
+			data, addr = proxy_socket.recvfrom(5000)
+			user_handler = UsersHandlers.get_by_ip(addr[0])
 
-		data = user_handler.cipher_aes.decrypt(data)
-		data_type = data[0]
+			data = user_handler.cipher_aes.decrypt(data)
+			data_type = data[0]
 
-		if data_type > 2:
-			user_handler.call_addresses[data_type - 3] = addr
-			continue
+			if data_type > 2:
+				user_handler.call_addresses[data_type - 3] = addr
+				continue
 
-		receiver_uid = int.from_bytes(data[1:5], "big")
+			receiver_uid = int.from_bytes(data[1:5], "big")
 
-		proxy_socket.sendto(data[5:], UsersHandlers.get(receiver_uid).call_addresses[data_type])
+			proxy_socket.sendto(data[5:], UsersHandlers.get(receiver_uid).call_addresses[data_type])
+		except Exception as e:
+			pass
